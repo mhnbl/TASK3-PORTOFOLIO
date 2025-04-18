ed645cc526b618c3da882db17c09fffaea2960e2
mhnbl <mhnbl0521@gmail.com>
2025-04-18 19:14:23 +0800

    Particle Backround

--- scripts.js
+++ scripts.js
@@ -278,3 +278,40 @@ function showMessage(event) {
       }, 1000);
     }
   }, 30);
+
+  particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ff0000" 
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.5
+      },
+      "size": {
+        "value": 3
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ff0000",
+        "opacity": 0.3,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2
+      }
+    },
+    "retina_detect": true
+  });
+  
